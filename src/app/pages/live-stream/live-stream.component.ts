1817c8ca6757226066daf5d4a37f6ca3e5b1ce93
N S Pious <nspious1999@gmail.com>
2024-12-21 19:21:06 +0530

    wupdate00

--- src/app/pages/live-stream/live-stream.component.ts
+++ src/app/pages/live-stream/live-stream.component.ts
@@ -58,7 +58,7 @@ export class LiveStreamComponent implements OnInit {
     // this.socket = io('https://streamiobackend.ddns.net/live-service');
 
 
-    this.socket = io('wss://streamiobackend.ddns.net', {
+    this.socket = io('https://streamiobackend.ddns.net', {
       path: '/live-service/socket.io', // Explicitly set the custom path
       transports: ['websocket', 'polling'], // Ensure compatibility with the server
       withCredentials: true, // Allow cross-origin cookies and credentials
